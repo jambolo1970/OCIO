1c1738a91331e6c01556f36667afc0ac93900660
Gianluca <jambolo1970@gmail.com>
2026-01-21 09:45:18 +0100

    Rename module and package references to 'OCIO'

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ocio",
+    name="OCIO",
     version="1.0.0",
     description="Promemoria per sbattere le palpebre e ridurre secchezza oculare",
     author="TuoNome",
     packages=find_packages(),
-    py_modules=["ocio"],
+    py_modules=["OCIO"],
     install_requires=[
         "Pillow>=11.0"
     ],
     entry_points={
         "console_scripts": [
-            "ocio=ocio:OcioApp"
+            "OCIO=OCIO:OcioApp"
         ]
     },
     include_package_data=True,
